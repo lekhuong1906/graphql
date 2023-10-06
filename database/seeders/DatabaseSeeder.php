85e9fda13281d7645119024b763a32d8b89a7a2b
unknown <lekhuong190602@gmail.com>
2023-10-06 17:35:48 +0700

    06/10

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Product;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +15,51 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        return [
+            Product::create([
+                [
+                    'title' => "Half of a Yellow Sun",
+                    'author' => "Chimamanda Ngozi Adichie",
+                    'language' => "English",
+                    'year_published' => "2006",
+                    'isbn' => "978-0-00-720028-3"
+                ],
+                [
+                    'title' => "Encyclopedia of Pseudoscience:",
+                    'author' => "William F. Williams",
+                    'language' => "English",
+                    'year_published' => "2000",
+                    'isbn' => "978-1-57958-207-4"
+                ],
+                [
+                    'title' => "Things fall apart",
+                    'author' => "Chinua Achebe",
+                    'language' => "English",
+                    'year_published' => "1958",
+                    'isbn' => "0385474547"
+                ],
+                [
+                    'title' => "Tall Tales about the Mind and Brain",
+                    'author' => "Della Sala",
+                    'language' => "English",
+                    'year_published' => "2007",
+                    'isbn' => "0-19-856877-0"
+                ],
+                [
+                    'title' => "Anthills of the Savannah",
+                    'author' => "Chinua Achebe",
+                    'language' => "English",
+                    'year_published' => "1987",
+                    'isbn' => "0385260458"
+                ],
+                [
+                    'title' => "Americanah",
+                    'author' => "Chimamanda Ngozi Adichie",
+                    'language' => "English",
+                    'year_published' => "2013",
+                    'isbn' => "978-0-307-96212-6"
+                ],
+            ])
+        ];
     }
 }
